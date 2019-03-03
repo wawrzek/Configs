66c35798356b2dcb6813ce614ce3a1496c929593
Wawrzyniec Niewodniczanski <wawrzek@darwin.lan>
2019-03-03 23:19:39 +0000

    Add vim to copy config

--- install-config.sh
+++ install-config.sh
@@ -1,8 +1,15 @@
 #!/bin/zsh
+
 configs=(zshenv zshrc zshprompt zssh zsh_functions zaws zlogout)
+vim=(vim vimrc gvimrc)
 for config in $configs
 do
 	cp $config ~/.$config
+done
+
+for config in $vim
+do
+	cp -r $config ~/.$config
 done
 
 sources=(zshenv zshrc)
