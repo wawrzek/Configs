e05968b813aad05a5e60f54aa32e3d8893b6ccf7
Wawrzyniec 'Wawrzek' Niewodniczański <wawrzek@r3.com>
2019-03-04 09:34:44 +0000

    Properly handle .vim dir

--- install-config.sh
+++ install-config.sh
@@ -4,15 +4,27 @@ function copy_config () {
 for config in $@
 do
 	echo Copying $config
-	cp -r $config ~/.$config
+	cp $config ~/.$config
 done
 }
 
 zsh_configs=(zshenv zshrc zshprompt zssh zsh_functions zaws zlogout)
-vim_configs=(vim vimrc gvimrc)
+vim_configs=(vimrc gvimrc)
 git_configs=(gitignore gitconfig)
 
 copy_config $zsh_configs $vim_configs $git_configs
+
+echo "Copying vim directory"
+VIMDIR=~/.vim
+if [ ! -d  $VIMDIR ]
+then
+	if [ -e  $VIMDIR ]
+	then
+		rm -f $VIMDIR
+	fi
+	mkdir $VIMDIR
+fi
+cp -R vim/ $VIMDIR
 
 sources=(zshenv zshrc)
 for config in $sources
