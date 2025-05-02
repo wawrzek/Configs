c2122073c5febb0e1efbe4687e554ea4d351a2ad
Wawrzyniec 'Wawrzek' Niewodniczański <main@wawrzek.name>
2025-05-02 19:09:29 +0100

    Update macos config

--- install-config.sh
+++ install-config.sh
@@ -8,7 +8,7 @@ do
 done
 }
 
-zsh_configs=(zshenv zshrc zssh zaws zazure zlogout)
+zsh_configs=(zshenv zshrc zssh zaws zazure zlogout zsh_$(uname))
 vim_configs=(vimrc gvimrc)
 git_configs=(gitignore gitconfig)
 
